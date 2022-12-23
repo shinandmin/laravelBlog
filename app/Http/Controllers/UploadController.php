ed9754915ec502647b050945b2072d7fb1116907
shinandmin <shinandmin13@naver.com>
2022-12-23 13:49:30 +0900

    '111'

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -9,11 +9,17 @@ class UploadController extends Controller
     public function upload(Request $request) {
         $file = $request->file( 'file' );
 
-        logger( $file );
-        logger( $file->isValid() );
-        logger( $file->path() );
-        logger( $file->extension() );
+        //logger( $file );
+        //logger( $file->isValid() );
+        //logger( $file->path() );
+        //logger( $file->extension() );
+        //logger( $file->getClientOriginalName() );
+        //logger( $file->getClientMimeType() );
 
-        return response()->json( [ 'message'=>'ok' ] );
+        $dir = date('y/m'); //20201212 날짜로 폴더 경로 생성
+        // storage->app->public->upload 폴더에 저장됨
+        $path = $file->store( 'uploads/' . $dir, 'public' );
+
+        return response()->json( [ 'path' => $path ] );
     }
 }
